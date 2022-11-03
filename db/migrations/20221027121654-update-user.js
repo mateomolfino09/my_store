602e55d1cfc233bfb67a6dc6adc4281a2ebedb85
Mateo Molfino <mmolfino@DESKTOP-Q9FK4AM.practia.uy>
2022-11-03 08:50:13 -0300

    mig

--- db/migrations/20221027121654-update-user.js
+++ db/migrations/20221027121654-update-user.js
@@ -1,10 +1,16 @@
 'use strict';
 const { USER_TABLE, UserSchema } = require('./../models/user.model');
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role)
+    await queryInterface.addColumn(USER_TABLE, 'role', {
+      allowNull: false,
+      type: DataTypes.STRING,
+      defaultValue: 'customer'
+    });
   },
 
   async down(queryInterface,) {
